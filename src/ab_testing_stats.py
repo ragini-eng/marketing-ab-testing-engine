3b539bebc87991756ea558237f86ec507817aaf2
Ragini  Pawaiya <raginipawaiya5@gmail.com>
2026-01-07 20:23:15 +0530

    Update and rename ab_testing_stats.py to src/ab_testing_stats.py

--- src/ab_testing_stats.py
+++ src/ab_testing_stats.py
@@ -10,12 +10,12 @@ print("Dataset loaded successfully")
 print("Variants found:", df['variant'].unique())
 
 # -----------------------------------
-# 1️⃣ Chi-square test for CTR (Clicked)
+# Chi-square test for CTR (Clicked)
 # -----------------------------------
 ctr_table = pd.crosstab(df['variant'], df['clicked'])
 chi2_ctr, p_ctr, dof, expected = chi2_contingency(ctr_table)
 
-print("\n📊 CTR Chi-square Test")
+print("\n CTR Chi-square Test")
 print("p-value:", round(p_ctr, 5))
 
 if p_ctr < 0.05:
@@ -24,12 +24,12 @@ else:
     print("Conclusion: No statistically significant difference in CTR between A and B")
 
 # -----------------------------------
-# 2️⃣ Chi-square test for Conversions
+#  Chi-square test for Conversions
 # -----------------------------------
 conv_table = pd.crosstab(df['variant'], df['converted'])
 chi2_conv, p_conv, dof, expected = chi2_contingency(conv_table)
 
-print("\n📊 Conversion Chi-square Test")
+print("\n Conversion Chi-square Test")
 print("p-value:", round(p_conv, 5))
 
 if p_conv < 0.05:
@@ -38,7 +38,7 @@ else:
     print("Conclusion: No statistically significant difference in conversion rate")
 
 # -----------------------------------
-# 3️⃣ T-test for Session Duration (Time)
+# T-test for Session Duration (Time)
 # -----------------------------------
 A_time = df[df['variant'] == 'A']['Time']
 B_time = df[df['variant'] == 'B']['Time']
@@ -46,7 +46,7 @@ B_time = df[df['variant'] == 'B']['Time']
 # Welch's T-test (safe for unequal variance)
 t_stat, p_time = ttest_ind(A_time, B_time, equal_var=False)
 
-print("\n📊 Session Duration T-test")
+print("\n Session Duration T-test")
 print("p-value:", round(p_time, 5))
 
 if p_time < 0.05:
@@ -55,9 +55,10 @@ else:
     print("Conclusion: No significant difference in session duration between A and B")
 
 # -----------------------------------
-# 4️⃣ Mean comparison (Business context)
+# Mean comparison (Business context)
 # -----------------------------------
 print("\n📈 Mean Session Duration Comparison")
 print("Variant A mean:", round(A_time.mean(), 3))
 print("Variant B mean:", round(B_time.mean(), 3))
 print("Difference (B - A):", round(B_time.mean() - A_time.mean(), 3))
+
